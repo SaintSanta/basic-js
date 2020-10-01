0832bcdbc393bf294455db0bd31231c09cd4ce2e
SaintSanta <62499815+SaintSanta@users.noreply.github.com>
2020-10-01 20:18:16 +0300

    fix:  uncommitted argument

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,5 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function countCats(/* matrix */) {
+module.exports = function countCats(matrix) {
   return matrix.flat().filter(element => element === '^^').length;
 };
