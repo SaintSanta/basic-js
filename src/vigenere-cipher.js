31df3bf665e8b424d971d6b9e3e3efb17bb95b10
SaintSanta <aleks2art@gmail.com>
2020-10-17 20:13:27 +0300

    feat: add solution

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,58 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+
+  constructor(direct = true) {
+    this.direct = direct;
+    this.alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  }
+
+  encrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error();
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let encrypted_message = "";
+    let keyPosition = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.indexOf(message[i]) === -1) {
+        encrypted_message += message[i];
+      } else {
+        encrypted_message += this.alphabet[(this.alphabet.indexOf(message[i]) + this.alphabet.indexOf(key[keyPosition])) % 26];
+        keyPosition = ++keyPosition % key.length;
+      }
+    }
+
+    if (this.direct) return encrypted_message;
+    return [...encrypted_message].reverse().join("");
+  }
+
+  decrypt(message, key) {
+    if (message === undefined || key === undefined) throw new Error();
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let decrypted_message = "";
+    let keyPosition = 0;
+
+    for (let i = 0; i < message.length; i++) {
+      if (this.alphabet.indexOf(message[i]) === -1) {
+        decrypted_message += message[i];
+      } else {
+        let position =
+          this.alphabet.indexOf(message[i]) - this.alphabet.indexOf(key[keyPosition]);
+        if (position < 0) position += 26;
+        decrypted_message += this.alphabet[position];
+
+        keyPosition = ++keyPosition % key.length;
+      }
+    }
+
+    if (this.direct) return decrypted_message;
+    return [...decrypted_message].reverse().join("");
   }
 }
 
